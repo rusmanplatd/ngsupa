c28db19416e41dcaf4aeacb2d7cb3c47e07c0b70
Rusman Wahab <rusmanplatd@gmail.com>
2026-06-18 16:16:19 +0700

    init

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  supabase: {
+    url: 'https://toigbmiflexnwtxpxlqi.supabase.co',
+    key: 'sb_publishable_EI7yrmkAJFiIH7WksNo_Kw_WTGKJ8-l',
+  },
+  firebase: {
+    apiKey: "AIzaSyAyZjxZyrfP-DuvXTmcpfXVtvST0TW4Qw8",
+    authDomain: "rusmanplatd.firebaseapp.com",
+    projectId: "rusmanplatd",
+    storageBucket: "rusmanplatd.appspot.com",
+    messagingSenderId: "537239787710",
+    appId: "1:537239787710:web:77ff809109969655b366ef",
+    measurementId: 'G-QQ4KPY3Z2N',
+    vapidKey: 'BJt2hcQAraH-E7nj0lI6YRfB06Tcv8l8Miavj7nGe84m-kfbXBj9KMvxI8hTDuZd9By_oKX3gzW6iEFKXGgLK24'
+  }
+};
